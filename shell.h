c4f5210650d017704b7bb75610e16a07bda91856
akinmode <akinjil@gmail.com>
2023-02-19 07:44:56 +0000

    Added headers to shell.h

---
+++ shell.h
@@ -0,0 +1,12 @@
+#ifndef _PROJECTSHELL_
+#define _PROJECTSHELL_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+/* PROTOTYPES */
+
+#endif
