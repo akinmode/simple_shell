7a9806a5d04d7e3a0a4998b7d9eeb3041c5024bf
akinmode <akinjil@gmail.com>
2023-02-22 22:59:27 +0000

    Mofidied main.c

--- main.c
+++ main.c
@@ -12,8 +12,8 @@ int main(int argc, char **argv)
 	do {
 		char *command = NULL, *delim = " \n";
 		char **tokens = NULL;
-		int status;
 		pid_t apid;
+
 		argc = 0;
 
 		printf("$ ");
@@ -27,22 +27,21 @@ int main(int argc, char **argv)
 		if (apid == -1)
 		{
 			perror("Error");
-			return (1);
+			return (-1);
 		}
 		else if (apid == 0)
 		{
 			if (execve(tokens[0], tokens, NULL) == -1)
 			{
 				perror(argv[0]);
-				return (1);
+				return (-1);
 			}
 		}
 		else
 		{
-			wait(&status);
+			wait(0);
 		}
 
-		
 		free(command), free(tokens);
 	} while (1);
 	return (0);
